9b95eb20b092a9b1a66ac413367777dbef636c16
Kzhong678 <kzhong678@gmail.com>
2021-11-28 14:32:03 -0600

    Change display text of a link

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ app.layout = html.Div(children=[
     ),
     html.A('Code on Github', href=githublink),
     html.Br(),
-    html.A("Link to Plot Doccumentation", href=sourceurl),
+    html.A("Plotly Link", href=sourceurl),
     ]
 )
 
